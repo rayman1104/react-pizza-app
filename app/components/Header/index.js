b01d06e83cff75f6511624fe4b5a641b533fd7c5
ray <rayman1104@gmail.com>
2018-04-10 12:41:28 +0300

    add Procfile

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 
 import Img from './Img';
-import NavBar from './NavBar';
-import HeaderLink from './HeaderLink';
 import Banner from './banner.jpg';
-import messages from './messages';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div>
         <Img src={Banner} alt="react-boilerplate - Logo" />
-        {/*
-        <NavBar>
-          <HeaderLink to="/">
-            <FormattedMessage {...messages.home} />
-          </HeaderLink>
-          <HeaderLink to="/menu">
-            <FormattedMessage {...messages.features} />
-          </HeaderLink>
-        </NavBar>
-        */}
       </div>
     );
   }
